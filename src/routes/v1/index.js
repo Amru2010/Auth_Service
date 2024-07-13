9e69caef96f1193f388c189698812db3ec7ef91c
Amru2010 <amrutanshjha2010@gmail.com>
2024-07-14 00:59:11 +0530

    Added email and password validation middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,12 @@
 const express=require('express');
 
 const UserController=require('../../controllers/user-controller');
+const {authValidator}=require('../../middlewares/index');
 
 const router=express.Router();
 
-router.post("/signup", UserController.create);
-router.post("/signin",UserController.signIn);
+router.post("/signup",authValidator, UserController.create);
+router.post("/signin",authValidator,UserController.signIn);
 
 
 
